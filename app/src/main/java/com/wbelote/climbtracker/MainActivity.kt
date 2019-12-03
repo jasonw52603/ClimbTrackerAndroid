97c1bc6ccd8585ca8f50e13775e26b0025715317
Will Belote <wbelote@unca.edu>
2019-12-03 12:26:53 -0500

    fixed navigation bar

--- app/src/main/java/com/wbelote/climbtracker/MainActivity.kt
+++ app/src/main/java/com/wbelote/climbtracker/MainActivity.kt
@@ -2,33 +2,44 @@ package com.wbelote.climbtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toolbar
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // setSupportActionBar(findViewById(R.id.mainToolbar))
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setSupportActionBar(findViewById(R.id.mainToolbar))
+        navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        val toolbar: Toolbar = findViewById(R.id.mainToolbar)
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//        findViewById<Toolbar>(R.id.mainToolbar)
+//            .setupWithNavController(navController, appBarConfiguration)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id == R.id.chooseGymFragment) {
-                toolbar.title = "Choose Gym"
+                mainToolbar.title = "Choose Gym"
             } else if (destination.id == R.id.chooseAreaFragment) {
-                toolbar.title = currentGym?.name ?: "No Gym"
+                mainToolbar.title = currentGym?.name ?: "No Gym"
             }
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     companion object {
